d1a3a81315cbf67c81b2955f49dc2c49b151e46f
Stephen Yao <stephenryao@gmail.com>
2020-07-12 21:59:34 +1000

    Added neighbors

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -4,6 +4,7 @@
  *  Description:
  **************************************************************************** */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Board {
@@ -14,7 +15,7 @@ public class Board {
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-        this.tiles = tiles.clone();
+        this.tiles = deepCopy(tiles);
     }
 
     // string representation of this board
@@ -78,24 +79,89 @@ public class Board {
         return Arrays.deepEquals(goal, this.tiles);
     }
 
-    // does this board equal y?
-    public boolean equals(Object y) {
-        Board board = (Board) y;
-        if (board == null) {
-            return false;
-        }
-
-        return Arrays.deepEquals(board.tiles, this.tiles);
-    }
-
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+        ArrayList<Board> list = new ArrayList<Board>();
+        for (int row = 0; row < tiles.length; row++) {
+            for (int column = 0; column < tiles.length; column++) {
+                int tile = tiles[row][column];
+                if (tile == 0) {
+                    int left = column - 1;
+                    int right = column + 1;
+                    int up = row - 1;
+                    int down = row + 1;
+
+                    Board leftNeighbor = swap(row, left, row, column);
+                    Board rightNeighbor = swap(row, right, row, column);
+                    Board topNeighbor = swap(up, column, row, column);
+                    Board bottomNeighbor = swap(down, column, row, column);
+
+                    if (leftNeighbor != null) list.add(leftNeighbor);
+                    if (rightNeighbor != null) list.add(rightNeighbor);
+                    if (topNeighbor != null) list.add(topNeighbor);
+                    if (bottomNeighbor != null) list.add(bottomNeighbor);
+                }
+            }
+        }
+
+        return list;
+    }
+
+    private Board swap(int row, int column, int currentRow, int currentColumn) {
+        if (row < 0 || column < 0 || row > dimension() - 1 || column > dimension() - 1) {
+            return null;
+        }
+        int[][] newTiles = deepCopy(tiles);
+        int swap = newTiles[row][column];
+        int tile = newTiles[currentRow][currentColumn];
+        newTiles[row][column] = tile;
+        newTiles[currentRow][currentColumn] = swap;
+        return new Board(newTiles);
+    }
+
+    private int[][] deepCopy(int[][] source) {
+        int[][] copy = new int[source.length][source.length];
+        for (int row = 0; row < source.length; row++) {
+            for (int col = 0; col < source.length; col++) {
+                copy[row][col] = source[row][col];
+            }
+        }
+        return copy;
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
         return null;
+    }
+
+    private static void test4Neighbors() {
+        int[][] tiles = { { 8, 1, 3 }, { 4, 0, 2 }, { 7, 6, 5 } };
+        Board board = new Board(tiles);
+        System.out.println("4 Neighbors:");
+        System.out.println(board.toString());
+        for (Board neighbor : board.neighbors()) {
+            System.out.println(neighbor.toString());
+        }
+    }
+
+    private static void test3Neighbors() {
+        int[][] tiles = { { 8, 1, 3 }, { 4, 6, 2 }, { 7, 0, 5 } };
+        Board board = new Board(tiles);
+        System.out.println("3 Neighbors:");
+        System.out.println(board.toString());
+        for (Board neighbor : board.neighbors()) {
+            System.out.println(neighbor.toString());
+        }
+    }
+
+    private static void test2Neighbors() {
+        int[][] tiles = { { 0, 1, 3 }, { 4, 6, 2 }, { 7, 8, 5 } };
+        Board board = new Board(tiles);
+        System.out.println("2 Neighbors:");
+        System.out.println(board.toString());
+        for (Board neighbor : board.neighbors()) {
+            System.out.println(neighbor.toString());
+        }
     }
 
     // unit testing (not graded)
@@ -111,10 +177,13 @@ public class Board {
         System.out.println(String.format("Manhattan distance: %d", board.manhattan()));
         System.out.println(
                 String.format("Is equal to same board: %b", board.equals(new Board(tiles))));
-        System.out.println(
-                String.format("Is equal to different board: %b",
-                              board.equals(new Board(otherTiles))));
-        
+        System.out.println(String.format("Is equal to different board: %b",
+                                         board.equals(new Board(otherTiles))));
+
+
+        test4Neighbors();
+        test3Neighbors();
+        test2Neighbors();
     }
 
 }
